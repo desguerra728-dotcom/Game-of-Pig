1cef2f4b828d8d847e323fec600c92d9f1bba4aa
desguerra728 <desguerra728@phm.k12.in.us>
2026-01-22 08:58:51 -0500

    j

--- gameOfPig.py
+++ gameOfPig.py
@@ -1,44 +1,70 @@
-'''
-PLAYER TURN
-while True
-    roll
-    if roll == 1:
-        return 0
-    turn_tot += roll
-    if p_tot + turn_tot > 100:
-        return turn_tot
-    if choice == bank:
-        return turn_tot
-p_tot += turn_tot
+import random
 
-COMPUTER TURN
-while True
-    roll
-    if roll == 1:
-        return 0
-    turn_tot += roll
-    if p_tot + turn_tot > 100:
-        return turn_tot
-    count += 1
-    if count == 20:
-        return turn_tot
-c_tot += turn_tot
+def dice(player):
+    roll = random.randint(1,6)
+    print(player + " rolled a " + str(roll))
+    return roll
 
-'''
+def bank_or_roll():
+    choice = random.randint(1,2)
+    if choice == 1:
+        return True
+    else:
+        return False
 
-import rnadom
+def ask():
+    while True:
+        choice = input("Bank or Roll?\n0:Bank\n1:Roll\n")
+        if choice == "0":
+            return True
+        elif choice == "1":
+            return False
+        else:
+            print("Enter 0 or 1: \n")
 
-def roll():
-    return random.randint()
 
 def player_turn(p_tot):
+    turn_tot = 0
+    isBanking = False
+    print("PLAYER TURN:")
     while True:
-        roll = roll()
-        if roll = 1:
+        roll = dice("Player")
+        if roll == 1:
             return 0
-        if 
+        turn_tot += roll
+        print("Turn points: " + str(turn_tot)+"\n")
+        if (turn_tot + p_tot) >= 100:
+            return turn_tot
+        isBanking = ask()
+        if isBanking == True:
+            return turn_tot
 
+def computer_turn(c_tot):
+    turn_tot = 0
+    print("COMPUTER TURN:")
+    while True:
+        roll = dice("Computer ")
+        if roll == 1:
+            return 0
+        turn_tot += roll
+        print("Turn points: " + str(turn_tot)+"\n")
+        if ((turn_tot + c_tot) >= 100):
+            return turn_tot
+        isBanking = bank_or_roll()
+        if isBanking == True:
+            return turn_tot
 
-While True:
-    p_tot = player_turn(p_tot)
-    
+p_tot = 0
+c_tot = 0
+print("Game Of Pig")
+while True:
+    p_tot += player_turn(p_tot)
+    print("Player total points: "+ str(p_tot) + "\n")
+    if p_tot >= 100:
+        print("Player wins.")
+        break
+    c_tot += computer_turn(c_tot)
+    print("Computer total points: "+ str(c_tot) + "\n")
+    if c_tot >= 100:
+        print("Computer wins.")
+        break
