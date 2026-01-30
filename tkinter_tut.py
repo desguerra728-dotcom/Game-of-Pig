8e1c67faa9406f2dab1c174495ea7f024a5e5d81
desguerra728 <desguerra728@phm.k12.in.us>
2026-01-30 09:49:59 -0500

    add color and change font

--- tkinter_tut.py
+++ tkinter_tut.py
@@ -4,20 +4,21 @@ clicked = False
 def say_hi():
     global clicked
     clicked = not clicked
+
     if clicked:
-        label.config(text = "Click Me")
+        label.config(text = "Click Me", bg = "green")
     else:
-        label.config(text = "You clicked the button")
+        label.config(text = "You clicked the button", bg = "blue")
 
 # create main window
 root = tk.Tk()
 root.title("My First TKinter Project")
 root.geometry("400x300")
 
-label = tk.Label(root, text = "Hello TKinter")
+label = tk.Label(root, text = "Hello TKinter", font = ("Arial", 28))
 label.pack()
 
-button = tk.Button(root, text = "Click Me", command = say_hi)
+button = tk.Button(root, text = "Click Me", command = say_hi, bg = "green", font = ("Arial", 28))
 
 button.pack()
 
